016de649a27d734a28385c4fae712c41713dda9c
Jon Bailey <jonathan.bailey@comcast.net>
2020-07-10 18:48:26 -0400

    Add OpenGL crate and get basic GLFW window implemented

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,62 @@
 extern crate glfw;
+use self::glfw::{Action, Context, Key};
 
-use glfw::{Action, Context, Key};
+extern crate gl;
+
+use std::sync::mpsc::Receiver;
+
+const WIDTH: u32 = 800;
+const HEIGHT: u32 = 600;
 
 fn main() {
-   let mut glfw = glfw::init(glfw::FAIL_ON_ERRORS).unwrap();
+    let mut glfw = glfw::init(glfw::FAIL_ON_ERRORS).unwrap();
+    glfw.window_hint(glfw::WindowHint::ContextVersion(3, 3));
+    glfw.window_hint(glfw::WindowHint::OpenGlProfile(
+        glfw::OpenGlProfileHint::Core,
+    ));
+    glfw.window_hint(glfw::WindowHint::OpenGlForwardCompat(true));
 
     // Create a windowed mode window and its OpenGL context
-    let (mut window, events) = glfw.create_window(800, 600, "Thermite Engine v0.1.0", glfw::WindowMode::Windowed)
+    let (mut window, events) = glfw
+        .create_window(
+            WIDTH,
+            HEIGHT,
+            "Thermite Engine v0.1.0",
+            glfw::WindowMode::Windowed,
+        )
         .expect("Failed to create GLFW window.");
 
     // Make the window's context current
     window.make_current();
     window.set_key_polling(true);
+    window.set_framebuffer_size_polling(true);
 
-    // Loop until the user closes the window
+    // load all OpenGL function pointers
+    gl::load_with(|symbol| window.get_proc_address(symbol) as *const _);
+
     while !window.should_close() {
-        // Swap front and back buffers
-        window.swap_buffers();
+        process_events(&mut window, &events);
 
-        // Poll for and process events
+        unsafe {
+            gl::ClearColor(0.2, 0.3, 0.3, 1.0);
+            gl::Clear(gl::COLOR_BUFFER_BIT);
+        }
+
         glfw.poll_events();
-        for (_, event) in glfw::flush_messages(&events) {
-            println!("{:?}", event);
-            match event {
-                glfw::WindowEvent::Key(Key::Escape, _, Action::Press, _) => {
-                    window.set_should_close(true)
-                },
-                _ => {},
+        window.swap_buffers();
+    }
+}
+
+fn process_events(window: &mut glfw::Window, events: &Receiver<(f64, glfw::WindowEvent)>) {
+    for (_, event) in glfw::flush_messages(events) {
+        match event {
+            glfw::WindowEvent::FramebufferSize(width, height) => unsafe {
+                gl::Viewport(0, 0, width, height)
+            },
+            glfw::WindowEvent::Key(Key::Escape, _, Action::Press, _) => {
+                window.set_should_close(true)
             }
+            _ => {}
         }
     }
 }

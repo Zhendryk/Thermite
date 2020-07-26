55a43660c3edf514159aaea758dab119b9a91254
Jon Bailey <jonathan.bailey@comcast.net>
2020-07-26 16:39:46 -0400

    Got basic window working with thermite_ui

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,25 @@
-// Conditionally compile the proper gfx backend as thermite_gfx_backend
-#[cfg(feature = "dx12")]
-use gfx_backend_dx12 as thermite_gfx_backend;
-// #[cfg(feature = "opengl")]
-// use gfx_backend_gl as thermite_gfx_backend;
-#[cfg(feature = "metal")]
-use gfx_backend_metal as thermite_gfx_backend;
-#[cfg(feature = "vulkan")]
-use gfx_backend_vulkan as thermite_gfx_backend;
-
 #[allow(unused_imports)]
 use log::{debug, error, info, trace, warn};
-use winit::{
-    self,
+use simple_logger;
+
+use thermite_ui::window;
+use thermite_ui::winit::{
     event::{Event, WindowEvent},
     event_loop::ControlFlow,
 };
 
-fn main() {}
-
-// fn main() {
-//     simple_logger::init().unwrap();
-//     let mut window = Window::default();
-//     // move forces the closure to take ownership of the captured data in its environment (e.g. window)
-//     window.event_loop().run(move |event, _, control_flow| {
-//         *control_flow = ControlFlow::Wait;
-//         match event {
-//             Event::WindowEvent {
-//                 event: WindowEvent::CloseRequested,
-//                 window_id,
-//             } if window_id == window.id() => *control_flow = ControlFlow::Exit,
-//             _ => (),
-//         }
-//     });
-// }
+fn main() {
+    simple_logger::init().expect("Couldn't create simple logger");
+    let mut window = window::Window::default();
+    // move forces the closure to take ownership of the captured data in its environment (e.g. window)
+    window.event_loop().run(move |event, _, control_flow| {
+        *control_flow = ControlFlow::Wait;
+        match event {
+            Event::WindowEvent {
+                event: WindowEvent::CloseRequested,
+                window_id,
+            } if window_id == window.id() => *control_flow = ControlFlow::Exit,
+            _ => (),
+        }
+    });
+}
---
+++ thermite_ui/src/lib.rs
@@ -0,0 +1,5 @@
+// Re-export winit to be used by clients of thermite_ui
+pub use winit;
+
+// thermite_ui modules
+pub mod window;
